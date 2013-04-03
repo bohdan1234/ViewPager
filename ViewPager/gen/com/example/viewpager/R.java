b1704539b3714d4548bcc2ba5aa9356346aefda7
bohdan <bohdan1234@gmail.com>
2013-04-03 17:58:48 +0300

    Fixed path

--- ViewPager/gen/com/example/viewpager/R.java
+++ ViewPager/gen/com/example/viewpager/R.java
@@ -30,10 +30,8 @@ public final class R {
         public static final int sample_5=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int awesomepager=0x7f080002;
-        public static final int button_add=0x7f080000;
-        public static final int button_remove=0x7f080001;
+        public static final int action_settings=0x7f080001;
+        public static final int awesomepager=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
